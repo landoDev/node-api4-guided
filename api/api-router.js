17744b24502eaadd24ca952fd98c31708d3a630b
Landon Turner <lturner@LTurner-MBP.lan1>
2020-04-09 13:44:24 -0400

    deploy changes message

--- api/api-router.js
+++ api/api-router.js
@@ -7,7 +7,8 @@ const router = express.Router();
 router.use(express.json());
 
 router.get("/", (req, res) => {
-  res.status(200).json({ api: "up" });
+  const message = this.process.env.MESSAGE || "hello from the yokel host"
+  res.status(200).json({ api: "up", message });
 });
 
 router.get("/shouts", (req, res, next) => {
