abddfe1277fb921292d720f02ed9447f27196133
Landon Turner <lturner@LTurner-MBP.lan1>
2020-04-09 12:39:20 -0400

    start script

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+
 const server = require("./api/server.js");
 
-const port = 5000;
+// make the port dynamic
+// The pipe OR operator allows for heroku to manage it but also lets me work locally because if process is undefined it will default to the right
+const port = process.env.PORT || 5000;
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
